2b6f9162e04f0a34161073b5735f64dc9cf1bdcf
Mejindra <55169933+Mejindra@users.noreply.github.com>
2020-02-02 13:18:50 -0600

    Add files via upload

---
+++ Assignment1/Assignment1/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Game
+    {
+        public string GameName { get; set; }
+        public int MaxNumPlayers { get; set; }
+
+        public Game (string gameName, int maxNumPlayers)
+        {
+            GameName = gameName;
+            MaxNumPlayers = maxNumPlayers;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Class info: {0}\nGame's Name: {1}\n Maximum Number of Player: {2}",
+                                  GetType(), GameName, MaxNumPlayers);
+                                
+        }
+
+    }
+}
---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game("Minecraft", 16);
+            var gameWithTimeLimit = new GameWithTimeLimit("Team Fortress 2", 32, 1000);
+
+            Console.WriteLine(game);
+            Console.WriteLine(gameWithTimeLimit);
+
+            Console.ReadLine();
+        }
+    }
+}
