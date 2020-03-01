2d2e1c8269881cf691aa8f675402214c35477fd8
Mejindra <55169933+Mejindra@users.noreply.github.com>
2020-03-01 15:16:35 -0600

    Add files via upload

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+         public class MainClass
+         {
+            public static void Main()
+            {
+                bool Equal = Calculator.AreEqual<string >("A", "A");
+                {
+                    if (Equal)
+                    {
+                        Console.WriteLine("Equal");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Equal");
+                    }
+                    Console.WriteLine("Press Enter to exit..........");
+                    Console.ReadKey();
+                }
+            }
+         }
+        public class Calculator
+        {
+            public static bool AreEqual<T>(T value1, T value2)
+            {
+                return value1.Equals(value2);
+            }
+
+        }
+        
+    }
+}
